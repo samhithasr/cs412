6a305b284aa3e5d624e4a2aefaffcb0ec766314a
Sam Somavarapu <samhitha@bu.edu>
2024-11-11 15:19:25 -0500

    Comments

--- voter_analytics/models.py
+++ voter_analytics/models.py
@@ -1,11 +1,17 @@
+# File: voter_analytics/models.py
+# Author: Samhitha Somavarapu (samhitha@bu.edu), 11/11/2024
+# Define the data objects for the voter_analytics application.
 from django.db import models
 
 # Create your models here.
 class Voter(models.Model):
     '''
-    Store/represent the data from one runner at the Chicago Marathon 2023.
-    BIB,First Name,Last Name,CTZ,City,State,Gender,Division,
-    Place Overall,Place Gender,Place Division,Start TOD,Finish TOD,Finish,HALF1,HALF2
+    Store/represent the data from one voter in Newton, MA.
+    Last name, first name, address number, street name, 
+    apartment number, ZIP code, date of birth, date of 
+    registration, party affiliation, precinct number, whether
+    or not voted in v20state; v21town; v21primary; v22general;
+    v23town, and voter score.
     '''
     # identification
     last = models.TextField()
@@ -33,21 +39,13 @@ class Voter(models.Model):
 def load_data():
     '''Load data records from a CSV file into model instances.'''
 
+    # Delete previous values (clean slate)
     Voter.objects.all().delete()
-    # open the file for reading:
-    # filename = "\Users\samso\Downloads\2023_chicago_results.csv"
     filename = "C:/Users/samso/django/newton_voters.csv"
 
     f = open(filename)
     headers = f.readline() # read/discard headers
     print(headers)
-    # line = f.readline() # read a line for processing
-    # fields = line.split(',') # create a list of fields
-    # print(fields)
-
-    # show the elements in this list of fields with index numbers
-    # for i in range(len(fields)):
-    #     print(f'fields[{i}] = {fields[i]}')
 
     # loop to read all the lines in the file
     for line in f:
@@ -56,7 +54,7 @@ def load_data():
         try:
             fields = line.split(',')
 
-            # create a new instance of Result object with this record from CSV
+            # create a new instance of Voter object with this info from CSV
             voter = Voter(last=fields[1].title(), first=fields[2].title(), adNumber=fields[3],
                             street=fields[4].title(), aptNum=fields[5].strip(), zipCode=fields[6], 
                             dob=fields[7], dor=fields[8], party=fields[9].strip(), 
@@ -66,6 +64,6 @@ def load_data():
    
             voter.save() # save to database
             print(f'Created voter: {voter}')
-        # handles the exception
+        # handles the exception and prints what it is
         except Exception as e:
             print(f"Exception on {fields}: {e}")
--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -1,3 +1,7 @@
+# File: voter_analytics/views.py
+# Author: Samhitha Somavarapu (samhitha@bu.edu), 11/11/2024
+# Description: The views file for the voter_analytics application.
+
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
@@ -17,18 +21,22 @@ class VotersListView(ListView):
     paginate_by = 100 # show 50 results per page
 
     def get_queryset(self) -> QuerySet[Any]:
-        '''Limit the voters to a small number of records'''
+        '''Limit the voters to a small number of voters'''
 
-        # default query set is all of the records:
+        # default query set is all of the voters, but get_qs here
+        # can filter for party, birth year, voter score, and if 
+        # a voter has voted in a specific election.
         qs = super().get_queryset()
         if 'party' in self.request.GET:
             party = self.request.GET['party']
             qs = qs.filter(party=party)
         if 'min' in self.request.GET:
             dob_min = self.request.GET.get('min')
+            # Chronological order
             qs = qs.filter(dob__year__gte=dob_min).order_by('dob')
         if 'max' in self.request.GET:
             dob_max = self.request.GET.get('max')
+            # Reverse chronological order
             qs = qs.filter(dob__year__lte=dob_max).order_by('-dob')
         if 'voter_score' in self.request.GET:
             voter_score = self.request.GET['voter_score']
@@ -49,7 +57,6 @@ class GraphListView(ListView):
     '''View to show graphs for voter data.'''
     template_name = 'voter_analytics/graphs.html'
     model = Voter
-    # context_object_name = 'voters'
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         '''Override the default get_context_data so that
@@ -63,9 +70,11 @@ class GraphListView(ListView):
             voters = voters.filter(party=party)
         if 'min' in self.request.GET:
             dob_min = self.request.GET.get('min')
+            # Given year is the minimum birth year
             voters = voters.filter(dob__year__gte=dob_min)
         if 'max' in self.request.GET:
             dob_max = self.request.GET.get('max')
+            # Given year is the maximum birth year
             voters = voters.filter(dob__year__lte=dob_max)
         if 'voter_score' in self.request.GET:
             voter_score = self.request.GET['voter_score']
@@ -82,7 +91,7 @@ class GraphListView(ListView):
             voters = voters.filter(v23town=True)
         context['voters'] = voters
 
-        # Get the list of dates of birth, and the years
+        # Get the list of dates of birth, and then the years
         dobs = voters.values_list('dob', flat=True)
         dob_years = [dob.year for dob in dobs]
 
@@ -95,8 +104,10 @@ class GraphListView(ListView):
         )
         bar_div = plotly.offline.plot(fig,auto_open=False,output_type='div')
 
+        # Pass it to context
         context['bar_div'] = bar_div
 
+        # Create counts for the pie chart
         parties = voters.values_list('party', flat=True)
         party_counts = {}
         for party in parties:
@@ -104,20 +115,23 @@ class GraphListView(ListView):
                 party_counts[party] += 1
             else:
                 party_counts[party] = 1
+        # Create labels for the pie chart
         x = list(parties.distinct())
-        # y = list(Voter.objects.values_list('party', flat=True))
-
+        
+        # Build pie chart
         fig2 = go.Figure(data=go.Pie(labels=x, values=list(party_counts.values())))
         fig2.update_layout(
             title='Party Affiliations Pie Chart',
         )
         pie_div = plotly.offline.plot(fig2, auto_open=False, output_type='div')
 
+        # Pass to context
         context['pie_div'] = pie_div
 
+        # Values for voter score histogram
         scores = voters.values_list('voter_score', flat=True)
-        # dob_years = [dob.year for dob in dobs]
 
+        # Build histogram for voter scores
         fig3 = go.Figure(data=[go.Histogram(x=list(scores))])
         fig3.update_layout(
             title='Voter Scores', 
@@ -126,6 +140,7 @@ class GraphListView(ListView):
         )
         bar2_div = plotly.offline.plot(fig3,auto_open=False,output_type='div')
 
+        # Pass to context
         context['bar2_div'] = bar2_div
 
         return context
